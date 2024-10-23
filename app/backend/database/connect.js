ba8c6f2a7379e5c38461ae4dcea1f35b5f8ddc5e
LukaszKrepa <lukaszkrepa@yahoo.com>
2024-10-23 13:58:18 +0200

    Finished articles

--- app/backend/database/connect.js
+++ app/backend/database/connect.js
@@ -2,7 +2,7 @@
 
 const { Sequelize} = require('sequelize')
 
-const sequelize = new Sequelize('devdb',process.env.DB_USER,process.env.DB_PASS,{
+const sequelize = new Sequelize('devdb',process.env.DB_USER,process.env.DB_PASSgit,{
     host: 'database-2.cv06umom2foy.eu-west-1.rds.amazonaws.com',
     dialect: "postgres"
 })
