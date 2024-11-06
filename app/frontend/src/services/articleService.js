4625691834bb98b4420da135f7e734d6f65efe30
LukaszKrepa <lukaszkrepa@yahoo.com>
2024-11-06 11:18:50 +0000

    added getArticle

--- app/frontend/src/services/articleService.js
+++ app/frontend/src/services/articleService.js
@@ -27,6 +27,32 @@ async function createArticle(articleData) {
     }
 }
 
-x = createArticle().then(x => console.log(x))
-module.exports = createArticle()
+async function getArticle(articleID) {
+    if (articleID == null){
+        throw new Error("Article data missing!")
+    }
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    try {
+        const response = await fetch(`${userAPI}articles/${articleID}`, requestOptions);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+
+module.exports = {createArticle,getArticle}
+
 
