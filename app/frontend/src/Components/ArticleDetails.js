11e741ec6ef3dcddc54efae6b4720c64ad2c4b21
LukaszKrepa <lukaszkrepa@yahoo.com>
2024-11-13 00:55:47 +0000

    Added order support

--- app/frontend/src/Components/ArticleDetails.js
+++ app/frontend/src/Components/ArticleDetails.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getArticle, getArticlePhotos } from '../services/articleService';
+import {createOrder} from "../services/orderService";
 
 const ArticleDetails = () => {
     const { id } = useParams();
@@ -35,16 +36,36 @@ const ArticleDetails = () => {
         return 'data:image/png;base64,' + window.btoa(binary);
     };
 
+    const handleBuy = async () => {
+        try {
+            const orderData = {
+                userID: id,
+                paymentMethodID : "4d530d77-217e-4a89-952e-f4cee8e3fe5c",
+                dateOfPurchase : Date.now(),
+                collectionMethod : "collection",
+                articles:{
+                    articleID: article.articleID,
+                }
+            };
+            const response = await createOrder(orderData);
+            alert('Order created successfully!');
+        } catch (error) {
+            console.error('Error creating order:', error);
+            alert('Failed to create order');
+        }
+    };
+
     // If article or imageUrl is not available, show loading
     if (!article) return <div>Loading...</div>;
 
     return (
         <div>
             <h1>{article.articleTitle}</h1>
-            <img src={imageUrl} alt={"cat"} />
+            <img src={imageUrl} alt={"cat"}/>
             <p>{article.description}</p>
             <p>Price: ${article.price}</p>
             <p>Status: {article.state}</p>
+            <button onClick={handleBuy}>Buy</button>
         </div>
     );
 };
