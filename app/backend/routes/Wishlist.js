3a42dfab7e662a337cc1da4cf614de178357069d
Eruloi01 <os23060@bristol.ac.uk>
2024-11-13 22:02:42 +0000

    route fix

--- app/backend/routes/Wishlist.js
+++ app/backend/routes/Wishlist.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const {getAllWishlists, createWishlist, getWishlist, updateWishlist, deleteWishlist} = require('../controllers/wishlists');
-const {updateUser} = require("../controllers/users");
 
 router.route('/').get(getAllWishlists).post(createWishlist);
 router.route('/:id').get(getWishlist).patch(updateWishlist).delete(deleteWishlist);
