c8879089b6ed4b8da3301b6143a1eb76cd1a5473
LukaszKrepa <lukaszkrepa@yahoo.com>
2025-01-29 11:05:53 +0000

    Added time recording for the search,login,buy,create tasks

--- app/frontend/src/Components/Home.js
+++ app/frontend/src/Components/Home.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import {getArticlePhotos, getArticles} from '../services/articleService';
-import './home.css'
+import { getArticlePhotos, getArticles } from '../services/articleService';
+import { createTaskLog } from '../services/logService';
+import './home.css';
 
 const Home = () => {
     const [articles, setArticles] = useState([]);
     const [inputValue, setInputValue] = useState('');
+    const [startTime, setStartTime] = useState(null);
 
     useEffect(() => {
         getArticles()
@@ -17,7 +19,7 @@ const Home = () => {
                         if (photosResponse && photosResponse.photos && photosResponse.photos[0]) {
                             const photoData = photosResponse.photos[0].image.data;
                             const uint8Array = new Uint8Array(photoData);
-                            const blob = new Blob([uint8Array], {type: 'image/png'});
+                            const blob = new Blob([uint8Array], { type: 'image/png' });
                             const reader = new FileReader();
                             return new Promise((resolve) => {
                                 reader.onloadend = () => {
@@ -34,114 +36,87 @@ const Home = () => {
                     console.error('No articles data found');
                 }
             })
-          .catch(error => {
-              console.error('Error fetching articles:', error);
-          });
-  }, []);
+            .catch(error => {
+                console.error('Error fetching articles:', error);
+            });
+    }, []);
 
-  const handleInputChange = (event) => {
-      setInputValue(event.target.value);
-  };
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleInputFocus = () => {
+        setStartTime(Date.now());
+    };
+
+    const handleArticleClick = async (articleID) => {
+        const endTime = Date.now();
+        const timeTaken = (endTime - startTime) ;
+        if (startTime){
+            await createTaskLog({
+                taskID: 4,
+                timeTaken: timeTaken,
+            });
+        }
+    };
 
     const filteredArticles = articles.filter(article =>
         article.articleTitle.toLowerCase().includes(inputValue.toLowerCase()) && article.state === "uploaded"
     );
 
-
-  return (
-    <div className="app">
-      <Header handleInputChange={handleInputChange} />
-      <div className="product-grid">
-        {filteredArticles.map((article, index)=>(
-          <ProductCard key={article.articleID} article={article}/>
-        ))}
-      </div>
-      <BottomNav />
-    </div>
-  );
-}
-
-function Header({handleInputChange}) {
-  return (
-    <div className="header">
-      <h1 className='title'>Circular Market System</h1>
-      <input type="text" className="search-bar" onChange = {handleInputChange} placeholder="Search" />
-      <button className="search-button">🔍</button>
-    </div>
-  );
-}
-
-/*
-function SearchBar() {
-    return(
-            <input>
-                type="text"
-                value={inputValue}
-                onChange={handleInputChange}
-                placeholder="Search articles..."
-            <input/>
-            {filteredArticles.length > 0 ? (
-                filteredArticles.map(article => (
-                    <div key={article.articleID}>
-                        <Link to={`/articles/${article.articleID}`}>
-                            <h2>{article.articleTitle}</h2>
-                        </Link>
-                        <p>Price: ${article.price}</p>
-                    </div>
-                ))
-            ) : (
-                <p>No articles found matching "{inputValue}"</p>
-            )}
-            <p>You entered: {inputValue}</p>
+    return (
+        <div className="app">
+            <Header handleInputChange={handleInputChange} handleInputFocus={handleInputFocus} />
+            <div className="product-grid">
+                {filteredArticles.map((article, index) => (
+                    <ProductCard key={article.articleID} article={article} onClick={() => handleArticleClick(article.articleID)} />
+                ))}
+            </div>
+            <BottomNav />
         </div>
-);
-};
-
-
- */
-function ProductCard({ article }) {
-  return (
-    <div className='product-card'>
-        {article.imageUrl ? (
-            <img src={article.imageUrl} alt={article.articleTitle} />
-        ) : (
-            <div className="product-image-placeholder">🖼️</div>
-        )}
-      <div className='product-info'>
-      <Link to ={`/articles/${article.articleID}`}>
-        <p className='product-name'>
-          {article.articleTitle}
-        </p>
-      </Link>
-      <p classname='product-price'>Price: {article.price}</p>
-      </div>
-      <button className='favorite-button'>❤</button>
-    </div>
-  )
-
-  //   <div className="product-card">
-  //     <Link to={`/articles/${article.articleID}`}>
-  //     <div className="product-image-placeholder">🖼️</div>
-  //     <div className="product-info">
-  //       <p className="product-name">{product.name}</p>
-  //       <p className="product-price">{product.price}</p>
-  //     </div>
-  //     <button className="favorite-button">❤</button>
-  //     </Link>
-  //   </div>
-  // );
+    );
 }
 
+function Header({ handleInputChange, handleInputFocus }) {
+    return (
+        <div className="header">
+            <h1 className='title'>Circular Market System</h1>
+            <input type="text" className="search-bar" onFocus={handleInputFocus} onChange={handleInputChange} placeholder="Search" />
+            <button className="search-button">🔍</button>
+        </div>
+    );
+}
+
+function ProductCard({ article, onClick }) {
+    return (
+        <div className='product-card' onClick={onClick}>
+            {article.imageUrl ? (
+                <img src={article.imageUrl} alt={article.articleTitle} />
+            ) : (
+                <div className="product-image-placeholder">🖼️</div>
+            )}
+            <div className='product-info'>
+                <Link to={`/articles/${article.articleID}`}>
+                    <p className='product-name'>
+                        {article.articleTitle}
+                    </p>
+                </Link>
+                <p className='product-price'>Price: {article.price}</p>
+            </div>
+            <button className='favorite-button'>❤</button>
+        </div>
+    );
+}
 
 function BottomNav() {
-  const navigate = useNavigate();
-  return (
-    <div className="bottom-nav">
-      <button className="nav-button" onClick={() => navigate('/')}>🏠</button>
-      <button className="nav-button" onClick={() => navigate('/CreateArticlePage')}>➕</button>
-      <button className="nav-button">👤</button>
-    </div>
-  );
+    const navigate = useNavigate();
+    return (
+        <div className="bottom-nav">
+            <button className="nav-button" onClick={() => navigate('/')}>🏠</button>
+            <button className="nav-button" onClick={() => navigate('/CreateArticlePage')}>➕</button>
+            <button className="nav-button">👤</button>
+        </div>
+    );
 }
 
 export default Home;
