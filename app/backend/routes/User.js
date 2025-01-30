7911764f34be44090df008ad8e59c905f38aa657
Karena <iv23220@bristol.ac.uk>
2025-01-30 11:48:30 +0000

    added Review route, ammended User route

--- app/backend/routes/User.js
+++ app/backend/routes/User.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllUsers, createUser, getUser, updateUser, deleteUser, userArticles, userOrders, loginUser} = require('../controllers/users');
+const {getAllUsers, createUser, getUser, updateUser, deleteUser, userArticles, userOrders, loginUser, userRating} = require('../controllers/users');
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 router.route('/:id/orders').get(userOrders);
 router.route('/:id/articles').get(userArticles);
+router.route('/:id/reviews').get(userRating)
 
 module.exports = router;
