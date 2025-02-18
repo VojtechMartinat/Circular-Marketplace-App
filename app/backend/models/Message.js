1ff3651f5df18ee6b49ef837d34e9394678fb307
LukaszKrepa <lukaszkrepa@yahoo.com>
2025-02-18 15:28:50 +0000

    Added the message model

---
+++ app/backend/models/Message.js
@@ -0,0 +1,40 @@
+
+const { DataTypes, Model } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Message extends Model {}
+
+    Message.init(
+        {
+            messageID: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                primaryKey: true,
+                defaultValue : DataTypes.UUIDV4
+            },
+            senderID: {
+                type: DataTypes.STRING(30),
+                allowNull: false,
+            },
+            receiverID: {
+                type: DataTypes.STRING(30),
+                allowNull: false,
+            },
+            articleID: {
+                type: DataTypes.UUID,
+                allowNull: false,
+
+            },
+            message: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'Article',
+        },
+    );
+
+    return Message;
+};
