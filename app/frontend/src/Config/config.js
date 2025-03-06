6d66a44740d8460680ed735a839e0b88ad5c3a5f
Vojtech Martinat <os23060@bristol.ac.uk>
2025-03-06 10:13:18 +0000

    correct url

--- app/frontend/src/Config/config.js
+++ app/frontend/src/Config/config.js
@@ -1,3 +1,3 @@
-const url = "http://relist.live.api/api/v1/"
+const url = "http://api.relist.live/api/v1/"
 const urlGateway = "https://4i0yaqji7g.execute-api.eu-west-1.amazonaws.com"
 module.exports = {url, urlGateway}
