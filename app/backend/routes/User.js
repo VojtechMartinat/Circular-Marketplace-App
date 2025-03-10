7d60ec66d98997268d8bd2db4f3607f9eb3551da
Karena <iv23220@bristol.ac.uk>
2025-03-10 16:54:22 +0000

    added controller and route to get all reviews on a user

--- app/backend/routes/User.js
+++ app/backend/routes/User.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const {getAllUsers, createUser, getUser, updateUser, deleteUser, userArticles, userOrders, loginUser, userRating,
-    userWrittenReviews, userTopUp, getInteractedUsers
+    userWrittenReviews, userReviews, userTopUp, getInteractedUsers
 } = require('../controllers/users');
 
 router.route('/').get(getAllUsers).post(createUser);
@@ -10,7 +10,8 @@ router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 router.route('/:id/orders').get(userOrders);
 router.route('/:id/articles').get(userArticles);
 router.route('/:id/rating').get(userRating);
-router.route('/:id/reviews').get(userWrittenReviews)
+router.route('/:id/writtenreviews').get(userWrittenReviews)
+router.route('/:id/reviews').post(userReviews);
 router.route('/:id/topup').post(userTopUp);
 router.route('/:id/chats').get(getInteractedUsers);
 module.exports = router;
